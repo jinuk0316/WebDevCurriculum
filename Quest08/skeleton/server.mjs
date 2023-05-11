fd4f4913dfb2ab189abf7547161e3f7afeb8f45c
Leejinuk <leejin7900@gmail.com>
2023-05-11 20:46:30 +0900

    [Quest last commit]

--- Quest08/skeleton/server.mjs
+++ Quest08/skeleton/server.mjs
@@ -1,11 +1,29 @@
 import http from "http";
+import url from "url";
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.write("Hello World!");
-  res.end();
+  const { pathname } = url.parse(req.url, true);
+  if (req.method === "POST" && pathname === "/foo") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      try {
+        const data = JSON.parse(body);
+        if (data && data.bar) {
+          res.writeHead(200, { "Content-Type": "text/plain" });
+          res.end(`Hello, ${data.bar}`);
+        }
+      } catch (err) {
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        res.end("Invalid JSON format");
+      }
+    });
+  } else {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World!");
+  }
 });
 
-server.listen(8080, () => {
-  console.log("Server is listening on port 8080");
-});
+server.listen(8080);
